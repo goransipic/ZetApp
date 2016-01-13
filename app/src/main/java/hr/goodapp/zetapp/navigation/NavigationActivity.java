fdbffaf8997d067596438173cdbe3be77f202f9e
shrenk <goran.sipic87@gmail.com>
2016-01-13 21:57:56 +0100

    navigationView edited

--- app/src/main/java/hr/goodapp/zetapp/navigation/NavigationActivity.java
+++ app/src/main/java/hr/goodapp/zetapp/navigation/NavigationActivity.java
@@ -69,15 +69,8 @@ public class NavigationActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
