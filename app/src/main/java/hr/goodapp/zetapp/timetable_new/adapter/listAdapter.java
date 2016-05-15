3fe1819bbb9dae12f96ba2550f0074b1809f20e6
shrenk <goran.sipic87@gmail.com>
2016-05-15 17:06:32 +0200

    another commit #2

--- app/src/main/java/hr/goodapp/zetapp/timetable_new/adapter/listAdapter.java
+++ app/src/main/java/hr/goodapp/zetapp/timetable_new/adapter/listAdapter.java
@@ -13,8 +13,6 @@ import hr.goodapp.zetapp.R;
  */
 public class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private static final int ITEM_TYPE_ONE = 0;
-    private static final int ITEM_TYPE_TWO = 1;
     String[] mArrayList;
 
     public ListAdapter(String[] arrayList) {
@@ -27,19 +25,9 @@ public class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         RecyclerView.ViewHolder viewHolder = null;
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
 
-        switch (viewType) {
 
-            case ITEM_TYPE_ONE:
-                View v1 = inflater.inflate(R.layout.timetable_type_one_layout, parent, false);
-                viewHolder = new ViewHolderTypeOne(v1);
-                break;
-
-            case ITEM_TYPE_TWO:
-                View v2 = inflater.inflate(R.layout.timetable_type_two_layout, parent, false);
-                viewHolder = new ViewHolderTypeTwo(v2);
-                break;
-
-        }
+        View v2 = inflater.inflate(R.layout.timetable_list_item, parent, false);
+        viewHolder = new ViewHolder(v2);
 
 
         return viewHolder;
@@ -48,16 +36,10 @@ public class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
-        switch (holder.getItemViewType()) {
-            case ITEM_TYPE_ONE:
-                ViewHolderTypeOne viewHolder = ((ViewHolderTypeOne) holder);
-                //viewHolder.getTextView().setText(mArrayList[position]);
-                break;
-            case ITEM_TYPE_TWO:
-                ViewHolderTypeTwo viewHolderTypeTwo = ((ViewHolderTypeTwo) holder);
-                viewHolderTypeTwo.getTextView().setText(mArrayList[position]);
-                break;
-        }
+
+        ViewHolder viewHolder = ((ViewHolder) holder);
+        viewHolder.getTextView().setText(mArrayList[position]);
+
     }
 
     @Override
@@ -65,41 +47,15 @@ public class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         return mArrayList.length;
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        if (position == 0) {
-            return ITEM_TYPE_ONE;
-        } else {
-            return ITEM_TYPE_TWO;
-        }
 
 
-    }
 
-    static public class ViewHolderTypeOne extends RecyclerView.ViewHolder {
+
+    static public class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView textView;
 
-        public ViewHolderTypeOne(View v) {
-            super(v);
-            //textView = (TextView) v.findViewById(R.id.bus_station);
-        }
-
-        public TextView getTextView() {
-            return textView;
-        }
-
-        public void setTextView(TextView textView) {
-            this.textView = textView;
-        }
-    }
-
-
-    static public class ViewHolderTypeTwo extends RecyclerView.ViewHolder {
-
-        private TextView textView;
-
-        public ViewHolderTypeTwo(View v) {
+        public ViewHolder(View v) {
             super(v);
             this.textView = (TextView) v.findViewById(R.id.timetable_type_two);
         }
