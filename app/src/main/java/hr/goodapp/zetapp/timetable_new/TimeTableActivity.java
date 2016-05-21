6de4d052f6deea4c1b7d2856189c1dbaecf83670
shrenk <goran.sipic87@gmail.com>
2016-05-21 13:20:31 +0200

    softkeyboard handling user input properly

--- app/src/main/java/hr/goodapp/zetapp/timetable_new/TimeTableActivity.java
+++ app/src/main/java/hr/goodapp/zetapp/timetable_new/TimeTableActivity.java
@@ -1,10 +1,17 @@
 package hr.goodapp.zetapp.timetable_new;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import hr.goodapp.zetapp.R;
 import hr.goodapp.zetapp.comon.adapter.DividerItemDecoration;
@@ -13,29 +20,60 @@ import hr.goodapp.zetapp.timetable_new.adapter.ListAdapter;
 
 public class TimeTableActivity extends AppCompatActivity {
 
+    private TextInputEditText mTextInputEditText;
+    private RecyclerView mRecyclerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_timetable);
 
+        initToolbar();
+
+        initViews();
+
+        mTextInputEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    v.clearFocus();
+                    handled = false;
+                }
+                return handled;
+            }
+        });
+
+        mTextInputEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    InputMethodManager imm =  (InputMethodManager)  getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        });
+
+
+    }
+
+    private void initViews() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_timetable);
+        mTextInputEditText = (TextInputEditText) findViewById(R.id.timetable_edit_text);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+
+        mRecyclerView.setAdapter(new ListAdapter(getResources().getStringArray(R.array.bus_times)));
+    }
+
+    private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView_timetable);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
-
-        recyclerView.setAdapter(new ListAdapter(getResources().getStringArray(R.array.bus_times)));
-
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-
-
-
 
 
 }
